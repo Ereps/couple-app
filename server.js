c9077e7ecb9266007f4be0a39cdd98f6b53e2e44
Ereps <erep23@hotmail.com>
2026-01-17 16:58:28 +0100

    New prompt

--- server.js
+++ server.js
@@ -4,6 +4,12 @@ import { generate_question } from "./generation.js";
 const PORT = process.env.PORT || 3000;
 let today_question = "";
 let date_message = new Date();
+let prompt = `Tu es un générateur de questions originales pour couples. 
+Ta mission est de proposer ** une seule question à la fois **, qui soit ** unique, amusante, profonde ou surprenante **, et qui incite le dialogue entre deux partenaires. 
+Évite absolument les répétitions par rapport aux questions précédentes. 
+Varie les thèmes: émotions, souvenirs, rêves, relations, humour, défis, intimité, créativité, valeurs personnelles, projets futurs. 
+Ne pose jamais de questions vagues ou génériques comme Comment ça va ? ou Qu'est-ce que tu aimes ?.
+Répond uniquement par la question, ** sans explications ni listes **.`;
 
 // async callback => await the response from generate_text
 const server = http.createServer(async (req, res) => {
@@ -13,10 +19,11 @@ const server = http.createServer(async (req, res) => {
     // check for the next day, if it's another month...
     try {
 
-      if (today_question === "" || new Date().getDay() < date_message.getDay()) {
-        today_question = await generate_question("Make a joke");
-        date_message = new Date();
-      }
+      //INFO: Removing the condition for debug
+      // if (today_question === "" || new Date().getDay() < date_message.getDay()) {
+      //   date_message = new Date();
+      // }
+      today_question = await generate_question(prompt);
       let response = {
         "question": JSON.stringify(today_question)
       }
